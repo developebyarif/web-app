039ae17ff799581f29e81a3aae6da2100d11628e
Arif <developebyarif@gmail.com>
2021-11-03 00:59:05 +0530

    chore: testing ci-cd pipeline rules

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>React JS Hosted Web app</p>
+        <p>Testing CI / CD Pipeline Rules.</p>
       </header>
     </div>
   );
